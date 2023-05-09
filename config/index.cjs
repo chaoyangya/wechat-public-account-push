3179f3cb12eedc5cf59dbd719cdbefdba5462da6
cy <42630564+chaoyangya@users.noreply.github.com>
2023-05-09 14:50:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,7 +19,7 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
 
-  
+   
 
   USERS: [
     {
@@ -28,7 +28,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o4PF-6VoxoOEXha9QlFCVKHEuYZc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'BkcrWSB_NkJJA0IBCaG5X5pW-tE8rF9F8KuXtMAdB60',
+      useTemplateId: 'lJkRT0A1VoScOknBKYUimGpqlmIIg8UkwRQzLzGR8Nk',
       PROVINCE: '上海',
       CITY: '上海',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -64,7 +64,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o4PF-6QqTrEYesPjb5kWmRdUxflk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'BkcrWSB_NkJJA0IBCaG5X5pW-tE8rF9F8KuXtMAdB60',
+      useTemplateId: 'lJkRT0A1VoScOknBKYUimGpqlmIIg8UkwRQzLzGR8Nk',
       PROVINCE: '北京',
       CITY: '北京',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
