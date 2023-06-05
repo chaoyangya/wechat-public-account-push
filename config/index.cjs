ac873fecdc5e9c2088f42a55ee884d9e5caf1e0a
cy <42630564+chaoyangya@users.noreply.github.com>
2023-06-05 10:43:20 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -46,9 +46,9 @@ const USER_CONFIG = {
             // 想要发送的人的名字
             name: '小王同学',
             // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-            id: 'SwagAlong',
+            id: 'o4PF-6QqTrEYesPjb5kWmRdUxflk',
             // 使用微信测试号：你想对他发送的模板消息的模板ID
-            useTemplateId: 'ZtK4KrqHfuDg29CBLMJdYwtZc_fDu7aLEtu9W3AtCTo',
+            useTemplateId: 'lJkRT0A1VoScOknBKYUimGpqlmIIg8UkwRQzLzGR8Nk',
             // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
             horoscopeDate: '01-29',
             festivals: [
